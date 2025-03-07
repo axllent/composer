e7c4ab1836da3ed0b30c89bba533c50ab8d791b2
Ralph Slooten <axllent@gmail.com>
2025-03-08 11:48:10 +1300

    Add PHP 7.4

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
-FROM alpine:3.21
+FROM alpine:3.15
 
 COPY install.sh /install.sh
 
 RUN apk add --no-cache bash && \
-    /install.sh 8.3 && \
+    /install.sh 7.4 && \
     apk del bash && \
     rm -f /install.sh
 
-COPY --from=composer /usr/bin/composer /usr/bin/composer
+COPY --from=composer:2.2 /usr/bin/composer /usr/bin/composer
 
 ENV COMPOSER_HOME=/tmp/composer \
     COMPOSER_CACHE_DIR=/tmp/composer-cache \
--- install.sh
+++ install.sh
@@ -14,38 +14,38 @@ MINOR="${semver[1]}"
 MM="${MAJOR}${MINOR}"
 
 apk add --no-cache \
-    php${MM}~=${PHP_VERSION} \
-    php${MM}-bcmath~=${PHP_VERSION} \
-    php${MM}-bz2~=${PHP_VERSION} \
-    php${MM}-common~=${PHP_VERSION} \
-    php${MM}-ctype~=${PHP_VERSION} \
-    php${MM}-curl~=${PHP_VERSION} \
-    php${MM}-exif~=${PHP_VERSION} \
-    php${MM}-fileinfo~=${PHP_VERSION} \
-    php${MM}-gd~=${PHP_VERSION} \
-    php${MM}-gettext~=${PHP_VERSION} \
-    php${MM}-gmp~=${PHP_VERSION} \
-    php${MM}-iconv~=${PHP_VERSION} \
-    php${MM}-intl~=${PHP_VERSION} \
-    php${MM}-mbstring~=${PHP_VERSION} \
-    php${MM}-opcache~=${PHP_VERSION} \
-    php${MM}-openssl~=${PHP_VERSION} \
-    php${MM}-pcntl~=${PHP_VERSION} \
-    php${MM}-phar~=${PHP_VERSION} \
-    php${MM}-posix~=${PHP_VERSION} \
-    php${MM}-session~=${PHP_VERSION} \
-    php${MM}-simplexml~=${PHP_VERSION} \
-    php${MM}-tokenizer~=${PHP_VERSION} \
-    php${MM}-xml~=${PHP_VERSION} \
-    php${MM}-xsl~=${PHP_VERSION} \
-    php${MM}-zip~=${PHP_VERSION}
+    php${MAJOR}~=${PHP_VERSION} \
+    php${MAJOR}-bcmath~=${PHP_VERSION} \
+    php${MAJOR}-bz2~=${PHP_VERSION} \
+    php${MAJOR}-common~=${PHP_VERSION} \
+    php${MAJOR}-ctype~=${PHP_VERSION} \
+    php${MAJOR}-curl~=${PHP_VERSION} \
+    php${MAJOR}-exif~=${PHP_VERSION} \
+    php${MAJOR}-fileinfo~=${PHP_VERSION} \
+    php${MAJOR}-gd~=${PHP_VERSION} \
+    php${MAJOR}-gettext~=${PHP_VERSION} \
+    php${MAJOR}-gmp~=${PHP_VERSION} \
+    php${MAJOR}-iconv~=${PHP_VERSION} \
+    php${MAJOR}-intl~=${PHP_VERSION} \
+    php${MAJOR}-mbstring~=${PHP_VERSION} \
+    php${MAJOR}-opcache~=${PHP_VERSION} \
+    php${MAJOR}-openssl~=${PHP_VERSION} \
+    php${MAJOR}-pcntl~=${PHP_VERSION} \
+    php${MAJOR}-phar~=${PHP_VERSION} \
+    php${MAJOR}-posix~=${PHP_VERSION} \
+    php${MAJOR}-session~=${PHP_VERSION} \
+    php${MAJOR}-simplexml~=${PHP_VERSION} \
+    php${MAJOR}-tokenizer~=${PHP_VERSION} \
+    php${MAJOR}-xml~=${PHP_VERSION} \
+    php${MAJOR}-xsl~=${PHP_VERSION} \
+    php${MAJOR}-zip~=${PHP_VERSION}
 
 # Symlink /usr/bin/php to /usr/bin/phpXY
 if [ ! -f "/usr/bin/php" ]; then
-    ln -s /usr/bin/php${MM} /usr/bin/php || exit 2
+    ln -s /usr/bin/php${MAJOR} /usr/bin/php || exit 2
 fi
 
 # Symlink /etv/php to /etc/phpXY
 if [ ! -h "/etc/php" ]; then
-    ln -s /etc/php${MM} /etc/php || exit 2
+    ln -s /etc/php${MAJOR} /etc/php || exit 2
 fi
